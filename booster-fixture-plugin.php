2492ba59e189f1111fc6007e271678baed236b2e
Benjamin Rush <bnjmnrsh@gmail.com>
2026-07-29 00:53:00 +0100

    feat: publish 0.2.0 fixture

--- booster-fixture-plugin.php
+++ booster-fixture-plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Booster Fixture Plugin
  * Description: Disposable public release fixture for RAN Booster integration tests.
- * Version: 0.1.0
+ * Version: 0.2.0
  * Requires at least: 6.5
  * Requires PHP: 8.2
  * Update URI: https://github.com/RocketsAreNostalgic/booster-fixture-plugin
