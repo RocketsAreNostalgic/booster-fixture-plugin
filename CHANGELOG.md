4a03ec7706aa03a3c0f9a7289831eb1eaf37af01
Benjamin Rush <bnjmnrsh@gmail.com>
2026-07-30 21:59:09 +0100

    build: freeze fixture release contract

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## 0.2.0
+
+- Publish the second disposable update fixture.
+
+## 0.1.0
+
+- Publish the initial disposable release fixture.
--- booster-fixture-plugin.php
+++ booster-fixture-plugin.php
@@ -2,7 +2,9 @@
 /**
  * Plugin Name: Booster Fixture Plugin
  * Description: Disposable public release fixture for RAN Booster integration tests.
+ * x-release-please-start-version
  * Version: 0.2.0
+ * x-release-please-end
  * Requires at least: 6.5
  * Requires PHP: 8.2
  * Update URI: https://github.com/RocketsAreNostalgic/booster-fixture-plugin
